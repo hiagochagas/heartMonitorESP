3ebec705a8bce7804297f77e61e41c914be12763
Hiago Chagas <hiago.chagas@premiersoft.net>
2023-04-16 14:18:03 -0300

    added confidentials file

---
+++ main/confidentials.c
@@ -0,0 +1,4 @@
+#include "confidentials.h"
+
+char WIFI_SSID[128] = "Insert your Wifi network name";
+char WIFI_PASSWORD[128] = "Insert your Wifi network password";
---
+++ main/confidentials.h
@@ -0,0 +1,5 @@
+#ifndef CONFIDENTIALS_H
+#define CONFIDENTIALS_H
+    extern char WIFI_SSID[128];
+    extern char WIFI_PASSWORD[128];
+#endif
