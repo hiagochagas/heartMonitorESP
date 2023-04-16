4711da3759454c93d4bba3f971ece60a3fccba17
Hiago Chagas <hiago.chagas@premiersoft.net>
2023-04-16 14:17:50 -0300

    added wifi connection

--- main/main.ino
+++ main/main.ino
@@ -1,5 +1,7 @@
 #include <Wire.h>
 #include "MAX30100_PulseOximeter.h"
+#include <ESP8266WiFi.h>
+#include "confidentials.h"
 
 #define REPORTING_PERIOD_MS     1000
 
@@ -12,6 +14,17 @@ uint32_t tsLastReport = 0;
 // Callback routine is executed when a pulse is detected
 void onBeatDetected() {
     Serial.println("♥ Beat!");
+}
+
+void setupWifi() {
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED) {
+      Serial.print(".");
+      delay(300);
+    }
+    Serial.println("Connected with IP: \n");
+    Serial.println(WiFi.localIP());
 }
 
 void setupOximeter() {
@@ -31,6 +44,7 @@ void setupOximeter() {
 
 void setup() {
     Serial.begin(115200);
+    setupWifi();
     setupOximeter();
 }
 
