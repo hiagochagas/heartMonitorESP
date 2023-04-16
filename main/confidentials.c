111b740520965266b5d220b33696de988f3457d4
Hiago Chagas <hiago.chagas@premiersoft.net>
2023-04-16 14:54:42 -0300

    added database url and api key to confidentials

--- main/confidentials.c
+++ main/confidentials.c
@@ -2,3 +2,5 @@
 
 char WIFI_SSID[128] = "Insert your Wifi network name";
 char WIFI_PASSWORD[128] = "Insert your Wifi network password";
+char DATABASE_URL[128] = "Firebase realtime database url";
+char API_KEY[128] = "Firebase API Key";
--- main/confidentials.h
+++ main/confidentials.h
@@ -2,4 +2,6 @@
 #define CONFIDENTIALS_H
     extern char WIFI_SSID[128];
     extern char WIFI_PASSWORD[128];
+    extern char DATABASE_URL[128];
+    extern char API_KEY[128];
 #endif
